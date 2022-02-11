855746ecc20f5e04560a70ecd7c521bbeb3f5bda
James Barry <jbm03003@gmail.com>
2022-02-11 14:00:29 -0500

    reworked api routes for functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,55 @@
 const router = require('express').Router();
 const fs = require('fs');
 
-router.get('/api/notes', function(req, res) {       
-    res.json(data);
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        noteData = JSON.parse(data);
+        res.send(noteData);
+    });
 });
 
-router.get('/api/notes/:id', function(req, res) {
-    res.json(data[Number(req.params.id)]);
-});
 
-router.get('api/notes/:id', (req, res) => {
-    let newNote = req.body;
-    let uniqueId = (data.length).toString();
-    console.log(uniqueId);
-    newNote.id = uniqueId;
-    data.push(newNote);
-    
-    fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
-        if (err) throw (err);        
-    }); 
+router.post('/api/notes', (req, res) => {
+    const userData = req.body;
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      noteData = JSON.parse(data);
+      noteData.push(userData);
+      let num = 1;
+      noteData.forEach((note, index) => {
+        note.id = num;
+        number++;
+        return noteData;
+      });
+      console.log(noteData);
 
-    res.json(data); 
+      stringData = JSON.stringify(noteData);
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Note Saved!');
   });
 
+  router.delete('/api/notes/:id', (req, res) =>{
+      const deleteData = req.params.id;
+      console.log(deleteData);
+      fs.readFile('./db/db.json', (err, data) =>{
+          if (err) throw err;
+          noteData = JSON.parse(data);
+          for (let i = 0; i < noteData.length; i++) {
+              if (noteData[i].id === Number(deleteData)) {
+                  noteData.splice([i], 1);
+              }
+          }
+          console.log(noteData);
+          stringData = JSON.stringify(noteData);
+          fs.writeFile('./db/db.json', stringData, (err, data) => {
+              if (err) throw err;
+          });
+      });
+      res.status(204).send()
+  });
 
 module.exports = router;
